eddb8241130102482f1495ce248dfc7e2931b14f
philipycha <philip.yc.ha@gmail.com>
2016-09-14 17:22:58 -0700

    Figuring out Story Board

---
+++ Mafia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mafia
+//
+//  Created by Philip Ha on 2016-09-14.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mafia/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Mafia
+//
+//  Created by Philip Ha on 2016-09-14.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
